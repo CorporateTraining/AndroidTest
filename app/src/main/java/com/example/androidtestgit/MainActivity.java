65c8952dcb451dc54d125bbe182d1936cb51bbe5
mahuanfy <mahuanfy@foxmail.com>
2020-07-28 21:57:53 +0800

    [Huanhuan]: finish main activity jump page.

--- app/src/main/java/com/example/androidtestgit/MainActivity.java
+++ app/src/main/java/com/example/androidtestgit/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.androidtestgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginActivityButton = findViewById(R.id.login_activity);
+        loginActivityButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
